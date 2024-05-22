6380237f8f8297295769e1af22dafee7069709cb
Bruno Fernandes <bruno.fernandes@dedalus.eu>
2024-05-22 14:22:59 +0100

    make a start on hero__section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='px-2 sm:px-6 lg:px-10 border'>
+        <div>Hero</div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/redux/movies/MovieSinglePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieSinglePage = () => {
+  return (
+    <div>MovieSinglePage</div>
+  )
+}
+
+export default MovieSinglePage
