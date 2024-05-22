0fcf8b0ee8a1e93d58317c02ad5cc0d02afebeaf
Bruno Fernandes <bruno.fernandes@dedalus.eu>
2024-05-22 15:24:08 +0100

    add component layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='px-2 sm:px-6 lg:px-10 border'>
-        <div>Hero</div>
+    <section className='py-4 min-h-52 overflow-hidden mb-10'>
+        <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 border h-[450px]'>
+            <p>Hero section. Movie cards will display here as carousel</p>
+        </div>
     </section>
   )
 }
