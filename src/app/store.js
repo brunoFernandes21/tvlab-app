21924b61ca844517160a799c99bda33e7291c2b9
Bruno Fernandes <bruno.fernandes@dedalus.eu>
2024-05-23 11:16:00 +0100

    setup redux store

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import moviesReducer from "../redux/movies/moviesSlice"
+
+export default configureStore({
+    reducer: {
+        // movies: jobsReducer
+    }
+})
