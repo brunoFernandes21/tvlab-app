a8d85a519db550b7ff5463f5eef5a7b40706647c
Bruno Fernandes <bruno.fernandes@dedalus.eu>
2024-05-29 11:06:39 +0100

    fetch tv shows and show on page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import NotFound from "./pages/NotFound";
 import Movies from "./redux/movies/Movies";
-
+import TvShow from "./redux/tvshows/TvShows"
 function App() {
 
   return (
@@ -12,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Homepage/>}/>
       <Route path="/movies" element={<Movies/>}/>
+      <Route path="/tv-shows" element={<TvShow/>}/>
       <Route path="*" element={<NotFound/>}/>
 
     </Routes>
